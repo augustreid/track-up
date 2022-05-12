815fc2c080ca6a0a90a4a2c7cb766bd2f2380e10
August Reid <august.reid@kin.com>
2022-05-12 11:20:04 -0600

    Basic architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'track-up';
 }
+
+//To start:
+//Header
+//add ride component (like edit shopping list)
+//ride model
+  //date
+  //exercise type (select options or booleans for each?)
+  //tack or bareback -- booleans?
+  //walk time in minutes -- number
+  //trot / tolt time in minutes -- number
+  //canter time in minutes -- number
+  //notes / details -- string
+//displayRides component (like recipe list)
+//singleRide component (like recipe item)
+//ride details component? 
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddRideComponent } from './add-ride/add-ride.component';
+import { HeaderComponent } from './header/header.component';
+import { RidesListComponent } from './rides-list/rides-list.component';
+import { SingleRideComponent } from './single-ride/single-ride.component';
+import { RideDetailsComponent } from './ride-details/ride-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRideComponent,
+    HeaderComponent,
+    RidesListComponent,
+    SingleRideComponent,
+    RideDetailsComponent
   ],
   imports: [
     BrowserModule
