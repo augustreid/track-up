a588f4624d5cb7d998d999a0b0b689db278ef3cc
August Reid <august.reid@kin.com>
2022-06-18 17:40:12 -0600

    Add angular calendar sort of

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http"
+import { HttpClientModule } from "@angular/common/http";
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AddRideComponent } from './rides/add-ride/add-ride.component';
@@ -12,6 +16,7 @@ import { HorseComponent } from './horse/horse.component';
 import { AddHorseComponent } from './add-horse/add-horse.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DragCompComponent } from './drag-comp/drag-comp.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +28,19 @@ import { AppRoutingModule } from './app-routing.module';
     RidesComponent,
     HorseComponent,
     AddHorseComponent,
-    HomeComponent
+    HomeComponent,
+    DragCompComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+        NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
